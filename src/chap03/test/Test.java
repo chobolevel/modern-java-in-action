6e3f746f2929084c48fa92bc5a47426490e82ea9
chobolevel <rodaka123@naver.com>
2023-05-04 19:52:06 +0900

    2023-05-04

--- src/chap03/test/Test.java
+++ src/chap03/test/Test.java
@@ -1,5 +1,21 @@
 package chap03.test;
 
+import chap02.entity.Apple;
+
+import java.util.Comparator;
+
 public class Test {
+
+    // chapter 03 정리
+
+//    1단계 코드
+//    public class AppleComparator implements Comparator<Apple> {
+//        @Override
+//        public int compare(Apple a1, Apple a2) {
+//            return a1.getWeight().compareTo(a2.getWeight());
+//        }
+//    }
+
+
 
 }
